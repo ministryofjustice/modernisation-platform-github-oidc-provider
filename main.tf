d46ea6db6f4017142c8e97187d6b68ad4170a694
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-07-29 08:33:03 +0100

    Intermediate commit to pair with the team later in the day

---
+++ main.tf
@@ -0,0 +1,8 @@
+
+# This module configures an OIDC provider for use with GitHub actions.
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  url             = "https://token.actions.githubusercontent.com"
+  tags            = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "application_name" {
   type        = string
   description = "Name of application"
 }
+
+variable "github_repository" {
+  type        = string
+  description = "The github repository, for example ministryofjustice/modernisation-platform-environments:*"
+}
+
+variable "additional_permissions" {
+  type        = aws_iam_policy_document
+  description = "accept aws_iam_policy_document with additional permissions to attach to the github-actions role"
+}
