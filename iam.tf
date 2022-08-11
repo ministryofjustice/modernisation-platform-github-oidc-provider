e7c28bf513544df946af50d4261ba7e663858359
David Sibley <david.sibley@digital.justice.gov.uk>
2022-08-11 14:26:32 +0100

    added missing resource for attaching extra permissions

--- iam.tf
+++ iam.tf
@@ -43,3 +43,8 @@ resource "aws_iam_policy" "extra_permissions" {
 
   policy = var.additional_permissions
 }
+
+resource "aws_iam_role_policy_attachment" "extra_permissions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.extra_permissions.arn
+}
