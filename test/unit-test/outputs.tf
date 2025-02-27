b2235e14f8d9767f2effcab3ab1987af409ff718
khatraf <khatra.farah@digital.justice.gov.uk>
2025-02-27 15:25:24 +0000

    save conflict

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -3,9 +3,7 @@ output "github_actions_provider" {
 }
 
 output "github_actions_trust_policy_conditions" {
-  output "github_actions_trust_policy_conditions" {
-    value = [for s in jsondecode(module.module_test.github_actions_role_trust_policy).Statement : try(s.Condition.StringLike, null)]
-  }
+  value = [for s in jsondecode(module.module_test.github_actions_role_trust_policy).Statement : try(s.Condition.StringLike, null)]
 }
 
 output "oidc_role" {
