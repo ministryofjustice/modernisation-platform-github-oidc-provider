e8c413f6db020efa13397589c100836ba1df3619
khatraf <khatra.farah@digital.justice.gov.uk>
2025-02-28 16:07:09 +0000

    .

--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -5,9 +5,10 @@ output "github_actions_provider" {
 output "github_actions_trust_policy_conditions" {
   value = [
     for s in jsondecode(module.module_test.github_actions_role_trust_policy).Statement :
-    { string_like = lookup(s.Condition, "StringLike", null) }
+    { string_like = try(s.Condition.StringLike, null) }
   ]
 }
+
 
 output "oidc_role" {
   value = module.module_test.github_actions_role
